7acae24b3cce5ef4b9a09810598963bd4a2fbf70
Fassih Haider <fassih.haider786@gmail.com>
2021-11-05 04:01:41 +0500

    refactor: :coffin: removes unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
-import { Event } from './events/entities/event.entity';
 import { EventsModule } from './events/events.module';
 
 @Module({
